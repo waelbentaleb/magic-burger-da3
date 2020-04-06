c5792a169bd96b12193fbb40ea8b263cb58aa691
Wael Ben Taleb <waelben7@gmail.com>
2020-04-06 18:03:39 +0100

    change app name and favicon

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ import logo from '../../assets/images/logo.png'
 const toolbar = () => {
   return (
     <div className={classes.Toolbar}>
-      <div>MENU</div>
+      {/* <div>MENU</div> */}
       <div className={classes.Logo}>
         <img src={logo} alt="Magic Burger" />
       </div>
